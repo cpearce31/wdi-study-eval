ee1d7285536fea5711379f0cb4ac6965d8d9ecae
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-09-14 13:58:04 -0400

    Add diff-inspection functionality
    The script will now get diffs for each PR and prompt the user to review
    each one, as well as the PR body message.
    
    TODO: refactor HTTP requests to use `request`

--- bin/study-eval.js
+++ bin/study-eval.js
@@ -1,27 +1,36 @@
 #!/usr/bin/env node
 'use strict'
 
+require('colors')
 require('dotenv').load()
+const https = require('follow-redirects').https
+const request = require('request')
+const prompt = require('prompt-sync')({ sigint: true })
 
-const pullsPromise = function (devs) {
+const user = process.env.GHUSER
+const token = process.env.GHTOKEN
+const defaultComment = process.env.COMMENT
+const repo = process.argv[2]
+
+console.reset = function () {
+  return process.stdout.write('\x1Bc')
+}
+
+const extractPath = url => '/' + url.split('/').slice(3).join('/')
+
+const options = {
+  hostname: 'git.generalassemb.ly',
+  port: 443,
+  path: `/api/v3/repos/ga-wdi-boston/${repo}/pulls?state=all&per_page=100&sort=updated&direction=desc`,
+  method: 'GET',
+  auth: `${user}:${token}`,
+  headers: {
+    'User-Agent': 'curl/7.38.0'
+  }
+}
+
+const getPulls = function (devs) {
   return new Promise((resolve, reject) => {
-    const https = require('https')
-
-    const user = process.env.GHUSER
-    const token = process.env.GHTOKEN
-    const repo = process.argv[2]
-
-    const options = {
-      hostname: 'git.generalassemb.ly',
-      port: 443,
-      path: `/api/v3/repos/ga-wdi-boston/${repo}/pulls?state=all&per_page=100&sort=updated&direction=desc`,
-      method: 'GET',
-      auth: `${user}:${token}`,
-      headers: {
-        'User-Agent': 'curl/7.38.0'
-      }
-    }
-
     https.get(options, (res) => {
       let result = ''
 
@@ -42,7 +51,7 @@ const pullsPromise = function (devs) {
                 url: pull.url,
                 diff_url: pull.diff_url,
                 comment_url: pull.review_comments_url,
-                legit: true
+                body: pull.body
               }
             })
 
@@ -77,4 +86,75 @@ const developersPromise = new Promise((resolve, reject) => {
   input.pipe(parser)
 })
 
-developersPromise.then(pullsPromise).then(console.log)
+const getDiffs = function (pulls) {
+  return Promise.all(pulls.map(pull => {
+    return new Promise((resolve, reject) => {
+      options.path = extractPath(pull.diff_url)
+      https.get(options, (res) => {
+        let result = ''
+
+        res.on('data', data => {
+          result += data
+        })
+
+        if (res.statusCode !== 200) {
+          res.on('end', () => reject(result))
+        } else {
+          res.on('end', () => {
+            pull.diff = result.split(/\r?\n/)
+                              .filter(line => line[0] === '+' ||
+                                              line[0] === '-')
+                              .map(line => line[0] === '-' ? '=== response ===' : line)
+                              .join('\n')
+            resolve(pull)
+          })
+        }
+      }).on('error', e => reject(e))
+    })
+  }))
+}
+
+const badPulls = []
+
+const inspectDiffs = function (pulls) {
+  pulls.forEach(pull => {
+    console.reset()
+    console.log('=== PR MESSAGES ===\n'.red)
+    console.log('GHE Username:'.blue, pull.github)
+    console.log('PR Body:'.blue, pull.body)
+    const useDefault = prompt('Use default comment? (y/n/x) '.green, 'y')
+    if (useDefault === 'x') { process.exit() }
+    pull.useDefault = useDefault === 'y'
+  })
+  pulls.forEach(pull => {
+    console.reset()
+    console.log('=== PR DIFFS ===\n'.red)
+    console.log('PR diff:\n'.blue, pull.diff)
+    const isLegit = prompt('Is this a reasonable response? (y/n/x) '.green, 'y')
+    if (isLegit === 'x') { process.exit() }
+    pull.isLegit = isLegit === 'y'
+    if (!pull.isLegit) { badPulls.push(pull) }
+  })
+  return pulls
+}
+
+const commentAndClose = function (pulls) {
+  pulls.forEach(pull => {
+    if (!pull.useDefault) {
+      console.reset()
+      console.log('=== CUSTOM COMMENTS ===\n'.red)
+      console.log('GHE Username:'.blue, pull.github)
+      console.log('PR Body:'.blue, pull.body)
+      console.log('PR diff:\n'.blue, pull.diff)
+      pull.comment = prompt('Enter a custom comment for the above PR: ')
+    }
+  })
+}
+
+developersPromise
+  .then(getPulls)
+  .then(getDiffs)
+  .then(pulls => {
+    commentAndClose(inspectDiffs(pulls))
+  })
+  .catch(console.error)
