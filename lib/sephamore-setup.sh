cd4ebfdd02a106e278e4c2751d18f7b93d573386
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-09-26 16:51:35 -0400

    Fix string matching in `lib/sephamore-setup.sh`
    Bash is weird.

--- lib/sephamore-setup.sh
+++ lib/sephamore-setup.sh
@@ -4,17 +4,17 @@ RESULTSDIR=$3
 TEMPLATE=$4
 URL=$5
 
-mkdir -pv $RESULTSDIR/$COHORT/$REPO
+mkdir -p $RESULTSDIR/$COHORT/$REPO
 cd $RESULTSDIR/$COHORT/$REPO
 
 git clone $URL
 
 cd $REPO
 
-if [ $TEMPLATE = "node" ]; then
-  npm install
-elif [ $TEMPLATE = "ruby"]; then
-  bundle install
-else
-  echo "Invalid template. You probably want 'node' or 'ruby'."
+if [ "$TEMPLATE" = "node" ]; then
+  echo `npm install`
+fi
+
+if [ "$TEMPLATE" = "ruby"]; then
+  echo `bundle install`
 fi
