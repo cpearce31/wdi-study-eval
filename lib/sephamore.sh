669833c64bdf1008d67d3a45bbea450bc7af7798
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-09-26 22:03:56 -0400

    Fix Rake tests
    Previously, `lib/sephamore.sh` didn't properly handle Rake tests.
    Changed the conditional so that it properly runs and dumps the test
    results.

--- lib/sephamore.sh
+++ lib/sephamore.sh
@@ -6,14 +6,15 @@ RESULTSDIR=$5
 
 cd $RESULTSDIR/$COHORT/$REPO/$REPO
 
-if [ $TEMPLATE = "node" ]; then
-  RESULT="$(timeout 15s grunt test)"
-elif [ $TEMPLATE = "ruby"]; then
-  RESULT="$(timout 15s bin/rake test)"
-fi
-
+HEADER="=== $GITHUB ===\n\n"
 touch ../$GITHUB.txt
 
-HEADER="=== $GITHUB ===\n\n"
+if [ $TEMPLATE = "node" ]; then
+  RESULT="$(timeout 15s grunt test)"
+  echo "$HEADER$RESULT\n" > ../$GITHUB.txt
+fi
 
-echo "$HEADER$RESULT\n" > ../$GITHUB.txt
+if [ $TEMPLATE = "ruby" ]; then
+  RESULT="$(timeout 15s bundle exec rake test)"
+  echo "$HEADER$RESULT\n" > ../$GITHUB.txt
+fi
